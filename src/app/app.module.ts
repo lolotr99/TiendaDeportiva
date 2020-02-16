908f32417c2702eededa9ec551d679f35a334d9b
Manolo Toro <manolotoro80@gmail.com>
2020-02-16 23:48:31 +0100

    Añado dependencias, componentes y navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { NotFoundComponent } from './componentes/not-found/not-found.component';
+import { PrivadoComponent } from './componentes/privado/privado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent,
+    NotFoundComponent,
+    PrivadoComponent
   ],
   imports: [
     BrowserModule,
